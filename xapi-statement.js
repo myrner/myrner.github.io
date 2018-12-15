041b725237bb006ac3cb1940306827a16ca8e851
Niels Myrner <nielsmyrner@gmail.com>
2018-12-15 12:34:31 -0800

    Storyline xAPI Statement
    Step 1. Done

---
+++ xapi-statement.js
@@ -0,0 +1,18 @@
+/* sample xapi statement - json object */
+
+{
+   "actor": {
+       "name": "Niels",
+       "mbox": "mailto:nielsmyrner@gmail.com"
+   }, 
+   "verb": {
+       "id": "http://activitystrea.ms/schema/1.0/complete",
+       "dispay": { "en-US": "completed" }
+   }, 
+   "object": {
+       "id": "http://www.devlinpeck.com/write-xapi-statement",
+       "definition": {
+           "name": { "en-US": "Write xAPI Statement Tutorial" }
+       }
+   }
+}
