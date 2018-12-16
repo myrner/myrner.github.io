1793c684c2ee31f1fe20abacea088cfdd47e7890
Niels Myrner <nielsmyrner@gmail.com>
2018-12-15 21:37:22 -0800

    Update xapi-statement.js

--- xapi-statement.js
+++ xapi-statement.js
@@ -7,11 +7,14 @@ function send_statement(verb, verbId, object, objectId) {
     var player = GetPlayer();
     var uNamejs = player.GetVar("uName");
     var uEmailjs = player.GetVar("uEmail");
+    
     var conf = {
       "endpoint": "https://trial-lrs.yetanalytics.io/xapi/",
       "auth": "Basic " + toBase64("0855df56592e3ea698bdb0ff12d9825c:4fffe16dcac56b04fa4bcd855793cbaf")
         };
+    
     ADL.XAPIWrapper.changeConfig(conf);
+    
     var statement = {
        "actor": {
            "name": uNamejs,
