3a89826d68afd73be760f83d615b30b2a9d48b2f
Niels Myrner <nielsmyrner@gmail.com>
2018-12-18 22:53:26 -0800

    xAPI statement frame

---
+++ xAPI/xAPI_statement.js
@@ -0,0 +1,27 @@
+function send_statement(verb, verbId, object, objectId) {
+  var player = GetPlayer();
+  var uNamejs = player.GetVar("uName");
+  var uEmailjs = player.GetVar("uEmail");
+  var conf = {
+    "endpoint": "https://trial-lrs.yetanalytics.io/xapi/",
+    "auth": "Basic " + toBase64("***KEY***:***SECRET***")
+  };
+  ADL.XAPIWrapper.changeConfig(conf);
+  var statement = {
+    "actor": {
+      "name": uNamejs,
+      "mbox": "mailto:" + uEmailjs
+    },
+    "verb": {
+      "id": verbId,
+      "display": { "en-US": verb }
+    },
+    "object": {
+      "id": objectId,
+      "definition": {
+        "name": { "en-US": object }
+      }
+    }
+  };
+  var result = ADL.XAPIWrapper.sendStatement(statement);
+} 
