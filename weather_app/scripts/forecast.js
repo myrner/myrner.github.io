a63750e49120ffde042a727420a6f9e98dd97f47
Niels Myrner <40707077+myrner@users.noreply.github.com>
2020-10-10 20:24:14 -0700

    update diversity landing page subtitle // resolve weather app mixed content block

--- weather_app/scripts/forecast.js
+++ weather_app/scripts/forecast.js
@@ -4,7 +4,7 @@ const key = '8ZUPGPNm6KTSHSmsndX0MZyJ58uk0gTr';
 // get weather information
 const getWeather = async (id) => {
 
-    const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+    const base = 'https://dataservice.accuweather.com/currentconditions/v1/';
     const query = `${id}?apikey=${key}`;
 
     const response = await fetch(base + query);
@@ -17,7 +17,7 @@ const getWeather = async (id) => {
 // get city information
 const getCity = async (city) => {
 
-    const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+    const base = 'https://dataservice.accuweather.com/locations/v1/cities/search';
     const query = `?apikey=${key}&q=${city}`;
 
     const response = await fetch(base + query);
